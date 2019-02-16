ad74251a8815a5d0364759eeed67ad842e2608f0
Karen Payne <paynekaren@comcast.net>
2019-02-16 13:01:21 -0800

    Added class project along with minor updates to existing code.

--- BestPracticeSqlServer2/DataClasses/DataOperations.cs
+++ BestPracticeSqlServer2/DataClasses/DataOperations.cs
@@ -5,7 +5,7 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using BestPracticeSqlServer1_library.ConnectionClass;
+using DataConnectionsLibrary.ConnectionClass;
 
 namespace BestPracticeSqlServer2.DataClasses
 {
---
+++ DataConnectionsLibrary/ConnectionClass/AccessConnection.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataConnectionsLibrary.GeneralClasses;
+using DataConnectionsLibrary.Interfaces;
+
+namespace DataConnectionsLibrary.ConnectionClass
+{
+    public abstract class AccessConnection : BaseExceptionProperties, IConnection
+    {
+        private readonly OleDbConnectionStringBuilder _builder = new OleDbConnectionStringBuilder { Provider = "Microsoft.ACE.OLEDB.12.0" };
+        /// <summary>
+        /// Database name and path
+        /// </summary>
+        protected string DefaultCatalog = "";
+        public string ConnectionString
+        {
+            get
+            {
+
+                if (string.IsNullOrWhiteSpace(DefaultCatalog))
+                {
+                    throw new Exception("Database name and path not provided.");
+                }
+
+                _builder.DataSource = DefaultCatalog;
+
+                return _builder.ConnectionString;
+
+            }
+        }
+        /// <summary>
+        /// Connection string with user name and password generated using the protection class
+        /// ProtectedDataConnections.ConnectionProtection
+        /// </summary>
+        public string ConnectionStringWithPassword { get; set; }
+    }
+}
